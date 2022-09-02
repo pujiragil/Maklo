7ff56f3b5e0f16ce04b1da21b0df12977e93e1b7
pujiragil <ragilsg164@gmail.com>
2022-09-02 22:04:42 +0700

    fix chart data issue

--- src/components/layout/chart/index.jsx
+++ src/components/layout/chart/index.jsx
@@ -1,5 +1,6 @@
+import "./chart.css"
 import { Line } from "react-chartjs-2"
-import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from "chart.js"
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Tooltip } from "chart.js"
 import { chartData } from "../../../data/chart-data"
 
 ChartJS.register(
@@ -7,20 +8,52 @@ ChartJS.register(
   LinearScale,
   PointElement,
   LineElement,
-  Title,
   Tooltip,
-  Legend
 )
 
 const options = {
   responsive: true,
   plugins: {
-    legend: {
-      position: 'top'
+    tooltip: {
+      position: 'nearest',
+      displayColors: false
+    }
+  },
+  scales: {
+    x: {
+      grid: {
+        display: false,
+        drawBorder: false
+      },
+      ticks: {
+        padding: 40
+      }
     },
-    title: {
+    y: {
       display: true,
-      text: 'title chart'
+      suggestedMin: 0,
+      suggestedMax: 10000,
+      grid: {
+        display: false,
+        drawBorder: false
+      },
+      ticks: {
+        stepSize: 2000,
+        padding: 40
+      }
+    }
+  },
+  layout: {
+    padding: {
+      left: -30
+    }
+  },
+  elements: {
+    point: {
+      radius: 10,
+      hoverRadius: 6,
+      pointBorderColor: 'transparent',
+      pointBackgroundColor: 'transparent',
     }
   }
 }
@@ -34,20 +67,24 @@ const data = {
     data: chartData.map(data => data.Income),
     cubicInterpolationMode: 'monotone',
     borderColor: 'rgb(43, 185, 46)',
-    backgroundColor: 'rgba(43, 185, 45, 0.5)'
+    backgroundColor: 'rgba(43, 185, 45, 0.5)',
+    hoverBackgroundColor: 'rgb(43, 185, 46)',
+    hoverBorderColor: '#FFF',
   }, {
     label: 'Expenses',
     data: chartData.map(data => data.Expenses),
     cubicInterpolationMode: 'monotone',
     borderColor: 'rgb(200, 238, 68)',
-    backgroundColor: 'rgba(200, 238, 68, 0.5)'
+    backgroundColor: 'rgba(200, 238, 68, 0.5)',
+    hoverBackgroundColor: 'rgb(200, 238, 68)',
+    hoverBorderColor: '#FFF',
   }]
 }
 
 
 export default function ChartLayout() {
   return (
-    <div>
+    <div className="chart--wrapper">
       <Line options={options} data={data}/>
     </div>
   )
